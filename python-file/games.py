d80597d3fcaf52b637612a869c6335a44994c9a6
Rand <randalsallaq97@gmail.com>
2020-12-25 20:12:37 +0200

    done by sondos and rand

---
+++ python-file/games.py
@@ -0,0 +1,12 @@
+from botato import *
+import spacy
+
+
+
+
+if __name__ == '__main__':
+    #botato functions
+    root = Little()
+    root.title('Little Kiddie')
+    root.mainloop()
+    #################
